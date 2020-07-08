a2689faaf0168cdf7919fd35376239db81ca01f0
Enkeli __ <programmistik@hotmail.com>
2020-07-08 20:40:21 +0400

    ---

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let modal = document.getElementById('myModal');
+//let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
+
+document.querySelectorAll('.myBtn').forEach(item => {
+    item.addEventListener('click', function () {
+        modal.style.display = "block";
+    })
+})
+
+
+//btn.onclick = function () {
+//    modal.style.display = "block";
+//}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
